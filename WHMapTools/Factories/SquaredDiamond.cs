e58d58ee330c5179d4347def955c39a2967de7dd
Jorge Ponce <daraciel@gmail.com>
2016-03-22 23:54:27 +0100

    asdds

--- WHMapTools/Factories/SquaredDiamond.cs
+++ WHMapTools/Factories/SquaredDiamond.cs
@@ -36,7 +36,15 @@ namespace WHMapTools.Factories
 
         public override IMap Create()
         {
-            throw new NotImplementedException();
+            this.resultMap = new HeightMap(this.Detail);
+
+            InitializeHeightMap();
+
+            Divide(this.resultMap.maxIterations);
+
+
+
+            return this.resultMap;
         }
 
         public override void Initialize(InitializeParams Params)
@@ -61,6 +69,15 @@ namespace WHMapTools.Factories
                 }
 
             }
+
+            if(this.Seed.HasValue)
+            {
+                rnd = new Random(this.Seed.Value);
+            }
+            else
+            {
+                rnd = new Random();
+            }
         }
 
         #endregion
@@ -76,9 +93,82 @@ namespace WHMapTools.Factories
 
         }
 
+        private void Divide(int size)
+        {
+            int x, y, half;
+            float offset;
+            half = size / 2;
+            float scale = this.Roughness * this.resultMap.side;
+            if (half >= 1)
+            {
+                for (y = half; y < this.resultMap.maxIterations; y += size)
+                {
+                    for (x = half; x < this.resultMap.maxIterations; x += size)
+                    {
+                        offset = rnd.Next() * scale * 2 - scale;
+                        Square(x, y, half, offset);
+                    }
+                }
+
+
+                for (y = 0; y <= this.resultMap.maxIterations; y += half)
+                {
+                    for (x = (y + half) % size; x <= this.resultMap.maxIterations; x += size)
+                    {
+                        offset = rnd.Next() * scale * 2 - scale;
+                        Diamond(x, y, half, offset);
+                    }
+                }
+
+                Divide(half);
+            }
+        }
+
+        private void Diamond(int x, int y, int size, float offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Square(int x, int y, int size, float offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        private float Average(float[] values)
+        {
+            float result = 0;
+            values = values.Where(p => p != -1).ToArray();
+            result = values.Sum();
+            result /
+
+            return result;
+        }
+
+        private void InitializeHeightMap()
+        {
+            this.setValue(0, 0, this.resultMap.maxIterations);
+            this.setValue(this.resultMap.maxIterations, 0, this.resultMap.maxIterations / 2);
+            this.setValue(this.resultMap.maxIterations, this.resultMap.maxIterations, 0);
+            this.setValue(0, this.resultMap.maxIterations, this.resultMap.maxIterations / 2);
+        }
+
         private void setValue(int x, int y, float value)
         {
             this.resultMap.heightmap[x + this.resultMap.side * y] = value;
+        }
+
+        private float getValue(int x, int y)
+        {
+            float result = -1;
+            if (x >= 0 && 
+                x <= this.resultMap.maxIterations && 
+                y >= 0 && 
+                y <= this.resultMap.maxIterations)
+            {
+                result = this.resultMap.heightmap[x + this.resultMap.side * y];
+            }
+
+            return result;
         }
 
         #endregion
