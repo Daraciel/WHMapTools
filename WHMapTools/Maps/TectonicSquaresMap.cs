4d0aa547d8192a431533c3b117d915ecd59b03ff
Jorge Ponce <daraciel@gmail.com>
2016-03-29 17:12:21 +0200

    Ejemplos
    Ejemplos y mejora del algoritmo añadida

---
+++ WHMapTools/Maps/TectonicSquaresMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using WHMapTools.Enums;
+using WHMapTools.Interfaces;
+
+namespace WHMapTools.Maps
+{
+    public class TectonicSquaresMap : IMap
+    {
+        public override Image Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
