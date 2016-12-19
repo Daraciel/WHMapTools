3ef40821b983440f3f190bdfdd8133ddb618fff1
Jorge Ponce <daraciel@gmail.com>
2016-12-19 20:55:37 +0100

    Generacion dinamica de pestañas
    Añadido generación dinamica de pestañas en GUI

--- WHMapTools.GUI/Form1.Designer.cs
+++ WHMapTools.GUI/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.tbMaps = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.NewMapTSMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.civilizationMapDesigner1 = new WHMapTool.UC.CivilizationMapDesigner();
             this.mainMenu.SuspendLayout();
             this.tbMaps.SuspendLayout();
@@ -41,18 +42,22 @@
             // 
             // mainMenu
             // 
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.archivoToolStripMenuItem});
             this.mainMenu.Location = new System.Drawing.Point(0, 0);
             this.mainMenu.Name = "mainMenu";
-            this.mainMenu.Size = new System.Drawing.Size(1028, 24);
+            this.mainMenu.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.mainMenu.Size = new System.Drawing.Size(1371, 28);
             this.mainMenu.TabIndex = 0;
             this.mainMenu.Text = "menuStrip1";
             // 
             // archivoToolStripMenuItem
             // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewMapTSMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
             this.archivoToolStripMenuItem.Text = "Archivo";
             // 
             // tbMaps
@@ -60,52 +65,64 @@
             this.tbMaps.Controls.Add(this.tabPage1);
             this.tbMaps.Controls.Add(this.tabPage2);
             this.tbMaps.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbMaps.Location = new System.Drawing.Point(0, 24);
+            this.tbMaps.Location = new System.Drawing.Point(0, 28);
+            this.tbMaps.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbMaps.Name = "tbMaps";
             this.tbMaps.SelectedIndex = 0;
-            this.tbMaps.Size = new System.Drawing.Size(1028, 546);
+            this.tbMaps.Size = new System.Drawing.Size(1371, 674);
             this.tbMaps.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.civilizationMapDesigner1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1020, 520);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1363, 645);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(833, 363);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1363, 645);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // NewMapTSMenuItem
+            // 
+            this.NewMapTSMenuItem.Name = "NewMapTSMenuItem";
+            this.NewMapTSMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.NewMapTSMenuItem.Text = "Nuevo Mapa";
+            // 
             // civilizationMapDesigner1
             // 
             this.civilizationMapDesigner1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.civilizationMapDesigner1.Location = new System.Drawing.Point(3, 3);
+            this.civilizationMapDesigner1.Location = new System.Drawing.Point(4, 4);
             this.civilizationMapDesigner1.Map = null;
+            this.civilizationMapDesigner1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.civilizationMapDesigner1.Name = "civilizationMapDesigner1";
-            this.civilizationMapDesigner1.Size = new System.Drawing.Size(1014, 514);
+            this.civilizationMapDesigner1.Size = new System.Drawing.Size(1355, 637);
             this.civilizationMapDesigner1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 570);
+            this.ClientSize = new System.Drawing.Size(1371, 702);
             this.Controls.Add(this.tbMaps);
             this.Controls.Add(this.mainMenu);
             this.MainMenuStrip = this.mainMenu;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.mainMenu.ResumeLayout(false);
             this.mainMenu.PerformLayout();
             this.tbMaps.ResumeLayout(false);
@@ -123,6 +140,7 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private WHMapTool.UC.CivilizationMapDesigner civilizationMapDesigner1;
+        private System.Windows.Forms.ToolStripMenuItem NewMapTSMenuItem;
     }
 }
 
--- WHMapTools.GUI/Form1.cs
+++ WHMapTools.GUI/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WHMapTool.UC;
 
 namespace WHMapTools.GUI
 {
@@ -15,6 +17,76 @@ namespace WHMapTools.GUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CreateMenuButtons();
+        }
+
+        private void CreateMenuButtons()
+        {
+            IEnumerable<Type> types;
+            types = GetTypesWithInterface(Assembly.Load("WHMapTool.UC"));
+            foreach (Type myType in types)
+            {
+                ToolStripMenuItem myItem = new ToolStripMenuItem();
+                myItem.Text = myType.Name;
+                myItem.Name = myType.Name + "TSMI";
+                myItem.Click += new EventHandler(delegate (Object o, EventArgs a)
+                {
+                    TabPage myTb = new TabPage();
+                    String name = String.Empty;
+                    String text = String.Empty;
+
+                    IMapDesigner myMapDesigner = (IMapDesigner)myType.GetConstructor(Type.EmptyTypes).Invoke(new object[] { });
+                    myTb.Controls.Add(myMapDesigner);
+                    name = "tb" + (tbMaps.TabPages.Count + 1);
+                    text = "New Map " + (tbMaps.TabPages.Count + 1);
+                    myTb.Name = name;
+                    myTb.Text = text;
+                    tbMaps.TabPages.Add(myTb);
+                });
+
+                this.NewMapTSMenuItem.DropDownItems.Add(myItem);
+
+                //(IMapDesigner x) => (IMapDesigner)mytype.GetConstructor(null).Invoke(null);
+            }
+
+            //foreach (Type mytype in WHMapTool.UC.GetTypes().Where(mytype => mytype.GetInterfaces().Contains(typeof(IMapDesigner))))
+            //{
+            //    TabPage myTb = new TabPage();
+            //    String name = String.Empty;
+            //    String text = String.Empty;
+                
+            //    IMapDesigner myMapDesigner = (IMapDesigner)mytype.GetConstructor(null).Invoke(null);
+            //    myTb.Controls.Add(myMapDesigner);
+            //    name = "tb" + (tbMaps.TabPages.Count + 1);
+            //    text = "New Map " + (tbMaps.TabPages.Count + 1);
+            //    myTb.Name = name;
+            //    myTb.Text = text;
+            //    tbMaps.TabPages.Add(myTb);
+            //}
+        }
+
+        private IEnumerable<Type> GetTypesWithInterface(Assembly asm)
+        {
+            var it = typeof(IMapDesigner);
+            return asm.GetTypes().Where(p => p.IsSubclassOf(it)).ToList();
+        }
+
+        private void sampleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage myTb = new TabPage();
+            String name = String.Empty;
+            String text = String.Empty;
+            IMapDesigner myMapDesigner = new CivilizationMapDesigner();
+            myTb.Controls.Add(myMapDesigner);
+            name = "tb" + (tbMaps.TabPages.Count + 1);
+            text = "New Map " + (tbMaps.TabPages.Count + 1);
+            myTb.Name = name;
+            myTb.Text = text;
+            tbMaps.TabPages.Add(myTb);
         }
     }
 }
